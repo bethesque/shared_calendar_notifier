aed5e6d6c2365903d3b41f2cf0859389e66c5c03
Bethany Skurrie <bethesk@gmail.com>
2016-11-25 18:26:05 +1100

    Sort events in date order

--- lib/shared_calendar_notifier/report.rb
+++ lib/shared_calendar_notifier/report.rb
@@ -3,8 +3,18 @@ module SharedCalendarNotifier
     def self.build_report for_user_email_address, events, created_after_date
       Report.new(
         :for_user_email_address => for_user_email_address,
-        :events => events.find_all{ | event| event.status != 'cancelled' && event.updated >= created_after_date && event.creator.email != for_user_email_address },
+        :events => filter_and_sort_events(for_user_email_address, events, created_after_date),
         :created_after_date => created_after_date)
+    end
+
+    def self.filter_and_sort_events for_user_email_address, events, created_after_date
+      events.find_all do | event|
+        event.status != 'cancelled' &&
+        event.updated >= created_after_date &&
+        event.creator.email != for_user_email_address
+      end.sort do | event_1, event_2 |
+        event_1.start.date_time <=> event_2.start.date_time
+      end
     end
   end
 
